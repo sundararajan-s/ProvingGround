3fc5380048773aea49da213950a0943333bff226
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2016-09-26 20:35:57 +0530

    hashCodes as lazy vals

--- core/src/main/scala/provingground/HoTT.scala
+++ core/src/main/scala/provingground/HoTT.scala
@@ -513,7 +513,7 @@ object HoTT {
   type Univ = Typ[Typ[Term]]
 
   trait BaseUniv {
-    override def hashCode = Type.hashCode
+    override lazy val hashCode = Type.hashCode
 
     override def equals(a: Any) = a match {
       case _: BaseUniv => true
@@ -750,7 +750,7 @@ object HoTT {
 
   class GenFuncTyp[W <: Term with Subs[W], U <: Term with Subs[U]](
       val domain: Typ[W], val fib: W => Typ[U]) {
-    override def hashCode = fib(domain.symbObj(HashSym)).hashCode() * 41 + 7
+    override lazy val hashCode = fib(domain.symbObj(HashSym)).hashCode() * 41 + 7
 
     override def equals(that: Any) = that match {
       case g: GenFuncTyp[u, v] =>
@@ -772,7 +772,7 @@ object HoTT {
 
     def asPi = PiTyp(lmbda("###" :: dom)(codom))
 
-//    override def hashCode = asPi.hashCode
+//    override lazy val hashCode = asPi.hashCode
 //
 //    override def equals(that: Any)=  that match {
 //      case FuncTyp(d, c) => (d == dom) && (c == codom)
@@ -1057,7 +1057,7 @@ object HoTT {
 
     def act(arg: X) = value.replace(variable, arg)
 
-    override def hashCode = {
+    override lazy val hashCode = {
       val newvar = variable.typ.symbObj(Name("hash"))
       val newval = value.replace(variable, newvar)
       41 * (variable.typ.hashCode + 41) + newval.hashCode
